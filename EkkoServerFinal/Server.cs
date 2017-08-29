1cb7d3dd4d024320d74ed23afa97b9ed570a97c0
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-08-29 13:18:46 +0200

    Oprettet sockets mellem client og server.

---
+++ EkkoServerFinal/Server.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EkkoServerFinal
+{
+    public class Server
+    {
+        public Server()
+        {
+
+        }
+
+        public void Start()
+        {
+            // TCP er den pålidelige forbindelse. TCP er et socket.
+
+            TcpListener server = new TcpListener(IPAddress.Loopback, 7);
+            server.Start();
+
+            //using funktionen sørger for at alt der åbnes lukkes pænt. 
+
+            using (TcpClient client = server.AcceptTcpClient())
+            using (NetworkStream ns = client.GetStream())
+
+            //En som læser.
+            using (StreamReader sr = new StreamReader(ns))
+            // En som skriver)
+            using (StreamWriter sw = new StreamWriter(ns))
+            {
+                // denne del adskiller vores ekko server fra en almindelig webserver.
+
+                string inlinje = sr.ReadLine();
+                Console.WriteLine("Server modtaget : " + inlinje);
+                sw.WriteLine(inlinje);
+
+                
+
+                // Flush tømmer det serveren har og sender ud til forbindelse. 
+                sw.Flush();
+
+                Console.ReadLine();
+            }
+
+        }
+    }
+
+}
+    
--- ServerClient/Program.cs
+++ ServerClient/Program.cs
@@ -4,15 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EkkoServerFinal
+namespace ServerClient
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Server server = new Server();
-            server.Start();
-
+            Client client = new Client();
+            client.Start();
         }
     }
 }
